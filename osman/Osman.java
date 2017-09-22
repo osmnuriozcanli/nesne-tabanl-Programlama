c2dddbde61b547a224e0be93c79f59378486dae6
osmnuriozcanli <osmannuri_ozcanli16@erdogan.edu.tr>
2017-09-22 19:59:24 +0300

    bir tane toplama metodu yazıldı

---
+++ osman/Osman.java
@@ -0,0 +1,26 @@
+/*
+ acıklama
+ */
+package osman;
+
+/**
+ *
+ * @author PC13 isim
+ */
+
+public class Osman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=50,b=2,c=0;
+        c=a+b;
+        System.out.println(topla (5,4));
+    }
+    
+    public static int topla (int k,int m) {
+        int toplam=k+m;
+        return toplam;
+    }
+}
